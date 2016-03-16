dc18585733f3f990c63a85619b5a9ac24e5af3a8
isntso <isntso@gmail.com>
2016-03-16 19:25:46 +0800

    上傳程式

---
+++ Homework1/Homework1/Problem2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Student someone =
+                new Student
+                {
+                    name = "老張",
+                    contact = new Contact
+                    {
+                        email = "resnick1223@gmail.com",
+                        phone = "0910123457"
+                    }
+                };
+
+            Console.WriteLine(someone.name);
+            Console.WriteLine(someone.contact.email);
+            Console.WriteLine(someone.contact.phone);
+        }
+    }
+}
